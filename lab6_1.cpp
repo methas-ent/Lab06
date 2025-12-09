2a9910dbad8d5e58cf3673f29a5506fcb30fde6d
Methas <methas_n@cmu.ac.th>
2025-12-10 00:03:52 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
+    int num,even=0,odd=0;
+    do{ 
+        cout << "Enter an integer: ";
+        cin >> num;
+    
+        if(num%2==0 && num!=0){
+            even++;
+        }else if (num%2!=0 && num!=0){
+            odd++;
+    }
+    
+    }while(num!=0);
+        
+    cout << "#Even numbers = "<<endl;
     cout << "#Odd numbers = ";
+    
     return 0;
 }
